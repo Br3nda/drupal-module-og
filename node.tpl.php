0a49ef9d13ac2cfb745edf37761cad827589d426
weitzman <weitzman>
2005-05-31 04:43:18 +0000

    sync with HEAD ... adding version numbers to some files

--- node.tpl.php
+++ node.tpl.php
@@ -1,3 +1,6 @@
+<?php // $Id: node.tpl.php,v 1.1.2.2 2005/05/31 04:43:18 weitzman Exp $
+?>
+
 <div class="node<?php print ($sticky) ? " sticky" : ""; ?>">
   <?php if ($page == 0): ?>
     <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
