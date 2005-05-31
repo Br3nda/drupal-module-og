4ef0a6c0a5cf42cf0926c723e84a85be292bf7f0
weitzman <weitzman>
2005-05-31 11:22:27 +0000

    only show groups and categories text when they exist

--- node-og.tpl.php
+++ node-og.tpl.php
@@ -1,10 +1,9 @@
-<?php //$Id: node-og.tpl.php,v 1.2 2005/05/31 04:43:34 weitzman Exp $ 
+<?php //$Id: node-og.tpl.php,v 1.3 2005/05/31 11:22:27 weitzman Exp $ 
 ?>
 <div class="node<?php print ($sticky) ? " sticky" : ""; ?>">
   <?php if ($page == 0): ?>
     <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
   <?php endif; ?>
-  <?php print $picture ?>
 
 
   <div class="content">
