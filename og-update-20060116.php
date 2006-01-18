5d0b370b8c49b6d8b770141702e0a4566264ce94
weitzman <weitzman>
2006-01-18 05:31:23 +0000

    Forward port from 4.6. Consolidate subscriber information in og_uid instead of node_access table. Simplifies the code quite a bit.
    A few other misc bugs fixed, such as wrong nodes in group feed.

---
+++ og-update-20060116.php
@@ -0,0 +1,17 @@
+<?php
+// $Id: og-update-20060116.php,v 1.1 2006/01/18 05:31:23 weitzman Exp $
+
+include_once "includes/bootstrap.inc";
+drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
+
+$sql = "DELETE FROM {node_access} WHERE realm = 'og_uid'";
+db_query($sql);
+
+$sql = "SELECT nid FROM {node} WHERE type = 'og'";
+$result = db_query($sql);
+while ($row = db_fetch_object($result)) {
+  $sql = "REPLACE INTO {node_access} (nid, gid, realm, grant_view, grant_update, grant_delete) VALUES (%d, %d, 'og_group', 1, 1, 1)";
+  db_query($sql, $row->nid, $row->nid);
+}
+
+?>
--- update-og.php
+++ update-og.php
@@ -1,13 +1,16 @@
 <?php
-// $Id: update-og.php,v 1.1 2005/10/20 10:20:30 killes Exp $
+// $Id: update-og.php,v 1.2 2006/01/18 05:31:23 weitzman Exp $
 include_once "includes/bootstrap.inc";
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 db_query("ALTER TABLE `og_uid` ADD `og_role` int(1) NOT NULL default '0'");
+db_query("ALTER TABLE `og_uid` ADD `is_active` int(1) default '0'");
+db_query("ALTER TABLE `og_uid` ADD `is_admin` int(1) default '0'");
 
 $result = db_query("SELECT * FROM {node_access} WHERE realm = 'og_uid' AND grant_view = 1");
 while ($object = db_fetch_object($result)) {
   $sql = "REPLACE INTO {og_uid} (nid, uid, og_role) VALUES (%d, %d, %d)";
   db_query($sql, $gid, $uid, ($object->grant_view + $object->grant_update));
 }
+
 ?>
