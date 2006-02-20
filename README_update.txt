e46347e070bc2a4081b752dc01e96623dfe428c8
weitzman <weitzman>
2006-02-20 06:22:31 +0000

    #49179 public posts should be shown on group home page to non subscribers

--- README_update.txt
+++ README_update.txt
@@ -1,9 +1,10 @@
-// $Id: README_update.txt,v 1.1.4.6 2006/02/11 23:40:28 weitzman Exp $
+// $Id: README_update.txt,v 1.1.4.7 2006/02/20 06:22:31 weitzman Exp $
 
 Reminder: please backup our node_access table before running this update.
 
-This script is safe to run from any 4.6 og prior feb 11.2006. if you ran an update script before, you might see errors on the ALTER TABLE statements. 
+This script is safe to run from any 4.6 og. if you ran an update script before, you might see errors on the ALTER TABLE statements. 
 Those can be safely skipped.
 
 Note:
 - this script outputs all its queries to the screen just for debugging. these are not errors. this is a safe update script and you can even run it many times if you were concerned that it did not run properly.
+- the feb 19 update reenables feature where public nodes appear on group home page for non subscribers
--- og-update-20060211.php
+++ og-update-20060211.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: og-update-20060211.php,v 1.1.2.1 2006/02/11 23:40:28 weitzman Exp $
+// $Id: og-update-20060211.php,v 1.1.2.2 2006/02/20 06:22:31 weitzman Exp $
 
 include_once "includes/bootstrap.inc";
 include_once 'includes/common.inc';
@@ -18,7 +18,7 @@ while ($object = db_fetch_object($result)) {
 $sql = "DELETE FROM {node_access} WHERE realm = 'og_uid'";
 db_queryd($sql);
 
-// feb 2006
+// feb 11, 2006
 $sql = "SELECT DISTINCT(n.nid) FROM {node} n INNER JOIN {node_access} na ON n.nid = na.nid WHERE type != 'og' AND na.realm = 'og_group'";
 $result = db_queryd($sql);
 while ($row = db_fetch_object($result)) {
@@ -32,5 +32,34 @@ while ($row = db_fetch_object($result)) {
   $sql = "REPLACE INTO {node_access} (nid, gid, realm, grant_view, grant_update, grant_delete) VALUES (%d, %d, 'og_group', 1, 1, 0)";
   db_queryd($sql, $row->nid, $row->nid);
 }
+
+// end feb 11, 2006
+
+// feb 19, 2006 - show public posts on group home page
+// add a row for each combination of public node and group. needed to make public nodes show up in group homepage for non subscribers
+$sql = "SELECT DISTINCT(nid) as nid FROM {node_access} WHERE realm = 'og_group' AND gid = 0" ;
+$result = db_queryd($sql);
+while ($row = db_fetch_object($result)) {
+  $sql = "SELECT gid FROM {node_access} WHERE nid = %d AND realm = 'og_group' AND gid != 0" ;
+  $result2 = db_queryd($sql, $row->nid);
+  while ($row2 = db_fetch_object($result2)) {  
+    $sql = "REPLACE INTO {node_access} (nid, realm, gid, grant_view) VALUE (%d, 'og_public', 0, %d)";
+    db_queryd($sql, $row->nid, $row2->gid); 
+  }
+}
+
+// change all former public node grants to 'og_all' realm
+$sql = "UPDATE {node_access} SET realm = 'og_all' WHERE realm = 'og_group' AND gid = 0 AND grant_view = 1";
+db_queryd($sql);
+
+// change all nodes in groups  to new 'og_subscriber' realm
+$sql = "UPDATE {node_access} SET realm = 'og_subscriber' WHERE realm = 'og_group' AND gid != 0";
+db_queryd($sql);
+
+// these records are no longer used. we've migrated them to new grant scheme
+$sql = "DELETE FROM {node_access} WHERE realm = 'og_group'";
+db_queryd($sql);
+
+// end feb 19
 
 ?>
