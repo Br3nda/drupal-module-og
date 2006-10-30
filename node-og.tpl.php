c39cbdaf1eaf08228d13947b2b8de29c86f747bf
weitzman <weitzman>
2006-10-30 04:41:42 +0000

    major commit. group home page is now powered by a View (as in Views.module). Views and Views_RSS modules are now required. A 'river of news' View is included, but 'Group by Content Type' is not (yet). Admins can now make own View to customize to taste. Soon, a group manager will choose own View ... No database change, so little risk in trying this out.

--- node-og.tpl.php
+++ node-og.tpl.php
@@ -1,13 +1,7 @@
-<?php //$Id: node-og.tpl.php,v 1.3 2005/05/31 11:22:27 weitzman Exp $ 
+<?php //$Id: node-og.tpl.php,v 1.4 2006/10/30 04:41:42 weitzman Exp $ 
 ?>
-<div class="node<?php print ($sticky) ? " sticky" : ""; ?>">
-  <?php if ($page == 0): ?>
-    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-  <?php endif; ?>
-
-
-  <div class="content">
-    <?php print $content ?>
-  </div>
-
+<div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
+  <?php if ($page == 0) { ?><h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2><?php }; ?>
+  <span class="taxonomy"><?php print $terms?></span>
+  <div class="content"><?php print $content?></div>
 </div>
