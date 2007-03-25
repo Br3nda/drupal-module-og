926bb474910fb1d4ba5eada1bf808a638e8ae4b0
weitzman <weitzman>
2007-03-25 16:15:29 +0000

    keep trying to get public checkbox right

--- og.js
+++ og.js
@@ -1,14 +1,14 @@
-// $Id: og.js,v 1.2 2007/03/04 15:49:58 weitzman Exp $
+// $Id: og.js,v 1.3 2007/03/25 16:15:29 weitzman Exp $
 
 Drupal.ogAttach = function() {
 /*  Disable the public checkbox if no groups are selected in in Audience*/
   $('.og-audience').click(function() {
     // Audience can be select or checkboxes
-    if ($('input.og-audience .form-checkbox')) {
+    if ($('input.og-audience.form-checkbox').size()) {
       var cnt = $('input.og-audience:checked').size();  
     }
     else {
-      var cnt = $('input.og-audience:selected').size();      
+      var cnt = $('select.og-audience option:selected').size();      
     }
     
       
