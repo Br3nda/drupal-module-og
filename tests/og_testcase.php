9b88fa8c430843fb5cc6627d7c505c01290f4dc4
weitzman <weitzman>
2009-04-09 21:14:20 +0000

    Significant patch by Moshe Weitzman. Please backup your database before upgrading ... Improve DB integrity. Factor the is_public column out of og_ancestry table and into a new og_access_post table that is 'owned' by og_access module. Add primary key to og_ancestry to prevent duplicate entries. Requires a visit to update.php. Customized Views which relied on the is_public column will need to be fixed in the Views UI. Views that ship with OG are unaffected.

--- tests/og_testcase.php
+++ tests/og_testcase.php
@@ -1,79 +1,115 @@
 <?php
+// $Id: og_testcase.php,v 1.10.4.1 2009/04/09 21:14:22 weitzman Exp $
 
-class OgTestCase extends DrupalTestCase {
-  var $_cleanupGroups;
-  
-  function addOg($type, $selective = OG_OPEN) {
-    
+/**
+ * @file
+ * Common functions for all the organic groups tests.
+ */
+
+class OgTestCase extends DrupalWebTestCase {
+  /**
+   * Create a group node.
+   *
+   * @param $type
+   *   The content type name.
+   * @param $selective
+   *   The group's visibility (e.g. open, moderated, etc').
+   * @return
+   *   The newly created node id.
+   */
+  function addOgGroup($type, $selective = OG_OPEN) {
+  	$edit = array();
+    $edit['og_description'] = $this->randomName(16);
+    $edit['og_selective'] = $selective;
+
+    // Keys that should be present when the node is loaded.
+    $keys = array(
+      'og_selective',
+      'og_description',
+      'og_theme',
+      'og_register',
+      'og_directory',
+      'og_language',
+      'og_private',
+    );
+    $og_type = t('Group node');
+    return $this->_addOgContent($type, $og_type, $edit, $keys);   	
+  }
+
+  /**
+   * Create a group post.
+   *
+   * @param $type
+   *   The content type name.
+   * @param $groups
+   *   An array with the group(s) id the post should belong to.
+   * @return
+   *   The newly created node id.
+   */
+  function addOgPost($type, $groups = array()) {
     $edit = array();
-    $edit['title']          = '!SimpleTest test group node! ' . $this->randomName(10);
-    $edit['og_description'] = '!SimpleTest og description' . $this->randomName(10);
-    $edit['body']           = '!SimpleTest test group welcome message! ' . $this->randomName(32) . ' ' . $this->randomName(32);
-    $edit['og_selective'] = (string)$selective;
-//    $this->_browser->setFieldByName('edit[og_theme]', ''); // May not be present, so no error catching
+    foreach ($groups as $gid) {
+      $edit['og_groups['. $gid .']'] = TRUE;
+    }
 
-    $this->drupalPostRequest("node/add/$type", $edit, 'Submit');
+    // Keys that should be present when the node is loaded.
+    $keys = array(
+      'og_groups',
+      'og_groups_both',
+    );
+    $og_type = t('Group post');
+    return $this->_addOgContent($type, $og_type, $edit, $keys);
+  }
 
-    $this->assertWantedRaw(t('Your %post has been created.', array ('%post' => $type)), 'Group created');
+  /**
+   * Helper function - create a group content.
+   *
+   * @param $type
+   *   The type name of the content type.
+   * @param $og_type
+   *   The og type - group or post.
+   * @param $edit
+   *   An array of settings to add to the defaults.
+   * @param $keys
+   *   An array with the keys that need to be present in the $node object
+   *   after node_load().
+   * @return
+   *   The newly created node id.
+   */
+  function _addOgContent($type, $og_type, $edit = array(), $keys = array()) {
+    $edit['title'] = $this->randomName(8);
+    $edit['body']= $this->randomName(32);
+    $type_hyphen = str_replace('_', '-', $type);
 
+    $this->drupalPost('node/add/'. $type_hyphen, $edit, t('Save'));
+
+    // Check that the form has been submitted.
+    $this->assertRaw(t('!type %title has been created.', array('!type' => $type, '%title' => $edit['title'])), t('%og_type created.', array('%og_type' => $og_type)));
+
+    // Assert the node has loaded properly.
     $node = node_load(array('title' => $edit['title']));
-    $this->assertNotNull($node, 'Group found in database. %s');
-    $this->_cleanupGroups[] = $node->nid;
-    
-    return $node->nid;
+    $node = (array)$node;
+    $this->assertTrue($this->assertKeysExist($keys, $node), t('%og_type loaded properly.', array('%og_type' => $og_type)));
+    // Node was casted to an array.
+    return $node['nid'];
   }
-  
-  function addNodeType() {
-    
-    $type = new stdClass();
 
-    $name = strtolower($this->randomName());
-    $type->type = trim($name);
-    $type->name = trim($name);
-    $type->orig_type = trim("");
-    $type->old_type = $type->type;
-  
-    $type->description    = $this->randomName(32, "description ... ");
-    $type->help           = $type->description = $this->randomName(32, "help ... ");
-    $type->min_word_count = 0;
-    $type->title_label    = "Title";
-    $type->body_label     = "Body";
-  
-    $type->module         = 'node';
-    $type->has_title      = $type->has_body = TRUE;
-    $type->custom         = "";
-    $type->modified       = TRUE;
-    $type->locked         = TRUE;
-  
-    $status = node_type_save($type);
-    $this->assertTrue(SAVED_NEW == $status, "Created node-type $name.");
-    
-    $this->_cleanupNodeTypes[] = $name;
-    
-    $types = variable_get('og_node_types', array());
-    $types[$name] = $name;
-    variable_set('og_node_types', $types);
-    
-    return $name;
-  }
-  
-  
-  function tearDown() {
-    while (sizeof($this->_cleanupGroups) > 0) {
-      $gid = array_pop($this->_cleanupGroups);
-      node_delete($gid);
+  /**
+   * Assert keys are in an array.
+   *
+   * @param $keys
+   *   An array of keys that needs to be checked.
+   * @param $array
+   *   The array that has the keys.
+   * @return
+   *   True if all keys exist.
+   */
+  function assertKeysExist($keys, $array) {
+    foreach ($keys as $key) {
+      if (!array_key_exists($key, $array)) {
+        return FALSE;
+      }
     }
-    
-    include_once './'. drupal_get_path('module', 'node') .'/content_types.inc';
-    while (sizeof($this->_cleanupNodeTypes) > 0) {
-      $name = array_pop($this->_cleanupNodeTypes);
-      
-      $types = variable_get('og_node_types', array());
-      unset($types[$name]);
-      variable_set('og_node_types', $types);
-
-      node_type_delete_confirm_submit(0, array('name' => $name, 'type' => $name));
-    }
-    parent::tearDown();
+    return TRUE;
   }
 }
